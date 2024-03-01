16f8931be0bd7ef753235c189de33ba96d7bd1d4
DavidTuma <dejvid.tuma@gmail.com>
2024-03-01 11:24:28 +0100

    create routes and new pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Dashboard from "./pages/Dashboard";
+import Main from "./pages/Main";
+import Analitics from "./pages/Analitics";
+import Fintech from "./pages/Fintech";
 
 const App = () => {
+  const activeMenu = true;
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <div className="flex relative dark:bg-main-dark-bg">
+        {activeMenu ? (
+          <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+            sidebar
+          </div>
+        ) : (
+          <div className='"w-0 dark:bg-secondary-dark-bg text-red-500'>
+            sidebar
+          </div>
+        )}
+      </div>
+      <div
+        className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+          activeMenu ? "md:ml-72" : "flex-2"
+        }`}
+      >
+        <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+          Navbar
+        </div>
+        <div>
+          <Routes>
+            {/* pages */}
+            <Route path="/dashboard" element={<Dashboard />}>
+              <Route path="main" element={<Main />} />
+              <Route path="analitics" element={<Analitics />} />
+              <Route path="fintech" element={<Fintech />} />
+            </Route>
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
